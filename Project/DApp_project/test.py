0530457e77646c5a34ef48e00857578c3dafe4b3
Albert-ChenZJ <64527216+Albert-ChenZJ@users.noreply.github.com>
2022-12-06 15:43:59 +0800

    Update test.py

--- Project/DApp_project/test.py
+++ Project/DApp_project/test.py
@@ -4,7 +4,7 @@ import json
 headers = {'content-type': 'application/json'}
 
 # res = requests.get(url='http://localhost:8000/invoke_hyperledger?key=cityu&value=foo',data=None,headers=headers)
-res = requests.get(url='http://localhost:8000/query_hyperledger?key=cityu',data=None,headers=headers)
+res = requests.get(url='http://localhost:8000/query_hyperledger?key=PolyU',data=None,headers=headers)
 
 
 print(res.json()['value'])
